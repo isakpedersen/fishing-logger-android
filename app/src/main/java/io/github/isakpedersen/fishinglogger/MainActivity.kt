4659da127f1aa958c95ddad51502e1e7fa3e3d81
Isak Pedersen <ispedersen5@gmail.com>
2026-07-08 12:15:32 +0200

    feat: add status screen with compose state

--- app/src/main/java/io/github/isakpedersen/fishinglogger/MainActivity.kt
+++ app/src/main/java/io/github/isakpedersen/fishinglogger/MainActivity.kt
@@ -4,24 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.isakpedersen.fishinglogger.ui.theme.FishingLoggerTheme
 
 class MainActivity : ComponentActivity() {
+    private var status by mutableStateOf("Starter...")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             FishingLoggerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    StatusScreen(
+                        status = status,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +37,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun StatusScreen(status: String, modifier: Modifier = Modifier) {
+    Column(
         modifier = modifier
-    )
+    ) {
+        Text("Status: $status")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun StatusScreenPreview() {
     FishingLoggerTheme {
-        Greeting("Android")
+        StatusScreen("SDK ready")
     }
 }
